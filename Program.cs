d0c66aa3909129be4f39d22639bcd71bb9a472fd
Kaustixx <janemorecraft@pm.me>
2026-05-05 13:06:28 +0700

    HOW THE FUCK DO I PARSE A JSON IN C SHARP (Will fix later)

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using Discord;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var Sharks = JsonSerializer.Deserialize<string>("blahaj.json");
+    }
+}
